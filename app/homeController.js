996fdc9ad5e8d469b29272d68285cdf1bb8478be
Sergi Bosch <sergiboschbosch@gmail.com>
2019-10-23 22:12:49 +0200

    Formateado

--- app/homeController.js
+++ app/homeController.js
@@ -7,32 +7,32 @@ var miControlador = miModulo.controller(
         $scope.filasPagina = pageRows;
 
         $http({
-            method:"GET",
+            method: "GET",
             withCredentials: true,
             url: "http://localhost:8081/blogbuster/json?ob=post&op=getcount"
-        }).then( function(response) {
-            var totalRows = response.data.message; 
+        }).then(function (response) {
+            var totalRows = response.data.message;
             var totalPages = totalRows / pageRows;
-            if(totalRows % pageRows > 0){
-                totalPages+=1;
+            if (totalRows % pageRows > 0) {
+                totalPages += 1;
             }
             var arrayPaginas = []
-            for (i=1;i<=totalPages;i++){
+            for (i = 1; i <= totalPages; i++) {
                 arrayPaginas.push(i);
             }
             $scope.paginas = arrayPaginas;
-         });
+        });
 
         $http({
-            method:"GET",
+            method: "GET",
             withCredentials: true,
-            url: "http://localhost:8081/blogbuster/json?ob=post&op=getpages&page="+page+"&rowPage="+pageRows
-        }).then( function(response) {
-            $scope.tablaPosts = response.data.message; 
-         });
+            url: "http://localhost:8081/blogbuster/json?ob=post&op=getpages&page=" + page + "&rowPage=" + pageRows
+        }).then(function (response) {
+            $scope.tablaPosts = response.data.message;
+        });
 
-         $scope.showSelectValue = function(mySelect) {
-            $window.location.href = "/blogbusterclient/BlogBuster-Client/#/1/"+mySelect;
+        $scope.showSelectValue = function (mySelect) {
+            $window.location.href = "/blogbusterclient/BlogBuster-Client/#/1/" + mySelect;
         }
     }]
 )
