392a607588201dc4529cfad9b38d5ef9519353a6
BOSCH BOSCH  SERGI <a041746825p@stretch-builder>
2019-10-28 08:53:16 +0100

    Apartado NEW añadido, aun por terminar

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,7 @@ miModulo.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/post/view/:id', { templateUrl: 'app/post/view/view.html', controller: 'postViewController' })
     $routeProvider.when('/post/remove/:id', { templateUrl: 'app/post/remove/remove.html', controller: 'postRemoveController' })
     $routeProvider.when('/post/edit/:id', { templateUrl: 'app/post/edit/edit.html', controller: 'postEditController' })
+    $routeProvider.when('/post/new', { templateUrl: 'app/post/new/new.html', controller: 'postNewController' })
 
     $routeProvider.otherwise({ redirectTo: '/home/1/10' })
 
