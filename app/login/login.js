1f5de54b8e9ceaa63d679b6cebb42413f5006f8c
BOSCH BOSCH  SERGI <a041746825p@stretch-builder>
2019-10-31 09:13:06 +0100

    Validaciones en los formularios

--- app/login/login.js
+++ app/login/login.js
@@ -1,9 +1,13 @@
 var miControlador = miModulo.controller(
     "loginController",
     ['$scope', '$http', '$routeParams', '$window', function ($scope, $http, $routeParams, $window) {
-        $scope.id = $routeParams.id;
         $scope.controller = "login";
 
-        
+        if($scope.username==""){
+
+        }
+        if($scope.password==""){
+            
+        }
     }]
 )
